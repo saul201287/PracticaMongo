da8bde8d9737e7a2917056145d558601aa16f7f2
JAL117 <106935663+JAL117@users.noreply.github.com>
2023-10-30 21:11:11 -0600

    Conexion

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+require("dotenv").config();
+mongoose.set("strictQuery", false);
+const port = 3006;
+
+app.listen(port, () => console.log("Escuchando en el puerto ", port));
+
+// Conexion a MongoDB
+mongoose
+  .connect("mongodb://localhost/Concesionaria", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Conectado a la base de datos"))
+  .catch((error) => {
+    console.error("Error de conexión a la base de datos:", error);
+  });
+
+module.exports = app;
