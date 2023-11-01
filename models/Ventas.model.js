5a0e786d6bcc073c7be2839fe379e21b46c2ca9d
JAL117 <106935663+JAL117@users.noreply.github.com>
2023-10-31 19:08:24 -0600

    Update Ventas.model.js

--- models/Ventas.model.js
+++ models/Ventas.model.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const ventaSchema = new mongoose.Schema({
-  fechaVenta: Date,
+  fechaVenta:{type:Date , require:true},
   cliente: { type: mongoose.Schema.Types.ObjectId, ref: 'Cliente' },
   vendedor: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendedor' },
   vehiculo: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehiculo' },
-  precioVenta: Number,
-  pagos: [{ fechaPago: Date, monto: Number }],
-  devoluciones: [{ fechaDevolucion: Date, motivo: String }],
+  precioVenta: { type: Number, require:true},
+  pagos: [{ fechaPago: Date, monto: Number ,require:true }],
+  devoluciones: [{ fechaDevolucion: Date, motivo: String , require:true}],
 });
 
 module.exports = mongoose.model('Ventas', ventaSchema);
