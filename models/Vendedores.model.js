872d9bc7d7374168503e5cea8cb4a93a5684d3f9
JAL117 <106935663+JAL117@users.noreply.github.com>
2023-11-01 00:07:04 -0600

    Actualizacion modelo vendedor

--- models/Vendedores.model.js
+++ models/Vendedores.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const vendedorSchema = new mongoose.Schema({
   nombre: {type: String , require: true},
   apellido: {type: String , require: true},
+  telefono: {type: Number , require: true}
   
 });
 
